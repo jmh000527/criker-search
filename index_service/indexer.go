19a7d351902938a7394b0c767d611cc4f7d391b1
jmh000527 <1003544211@qq.com>
2024-08-29 21:15:03 +0800

    Changes.

--- index_service/indexer.go
+++ index_service/indexer.go
@@ -2,7 +2,7 @@ package index_service
 
 import "github.com/jmh000527/criker-search/types"
 
-// Indexer Sentinel（分布式grpc的哨兵）和Indexer（单机索引）都实现了该接口
+// Indexer Sentinel（分布式grpc的哨兵）和 LocalIndexer（单机索引）都实现了该接口
 type Indexer interface {
 	AddDoc(doc types.Document) (int, error)
 	DeleteDoc(docId string) int
